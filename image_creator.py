11f93e06ab596d50052ed06b4745ecf99a6eaac4
Wojciech Malicki <wojciechmalicki@gmail.com>
2019-11-25 02:34:18 +0100

    add support command line's parametr

--- image_creator.py
+++ image_creator.py
@@ -1,47 +1,71 @@
 # -------------------------------------------------------------------------------
-# Name:        ranndom image creator
-# Purpose:     skrypt przygotowany w czasie wakacji 2018
+# Name:        random image creator
 #
 # Author:      Wojciech
 #
-# Created:     19.07.2018
-# Copyright:   (c) Wojciech 2018
+# Created:     25.11.2019
+# Copyright:   (c) Wojciech 2019
 # Licence:     Za darmo w celach edukacyjnych
 # -------------------------------------------------------------------------------
 
-import sys
+import argparse
 import datetime
+import os
 from random import randint as rint
 from random import choice as chc
-from random import randint as rint
 
+# set datestamp
 now = datetime.datetime.now()
-height = width = 600
-directory = 'output\\'
-filename = 'img_{:%d%m%Y%H%M%S}_{}x{}.raw'.format(now,width,height)
-pathname = directory + filename
 
+# default value
+value = [600, 600, 'output\\', 'img_[ddmmYYYYHHMMSS]_[width]x[height].raw', 'fc']
 
-if len(sys.argv) != 3:
-        print('Program wywołany z domyślnymi ustawieniami:')
-        print('-obraz 600px x 600px')
-        print('-obraz zostanie zapisany w katalogu {} pod nazwą {}.'.format(directory[:-1], filename))
-        print('Wywołanie z parametrami: python {} height width [path_to_folder]'.format(sys.argv[0]))
-else:
-    height = int(sys.argv[1])
-    width = int(sys.argv[2])
-    if len(sys.argv) == 4:
-        pathname = sys.argv[3] + filename
+# arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-hh", "--height", type=int, help=f"image's height in pixels (default: {value[0]}px)")
+parser.add_argument("-w", "--width", type=int, help=f"image's width in pixels (default: {value[1]}px)")
+parser.add_argument("-d", "--directory", type=str, help=f"path to file (default: {value[2]}")
+parser.add_argument("-f", "--filename", type=str, help=f"file name (default: {value[3]}")
+parser.add_argument("-c", "--colormode", type=str, help=f"mode of color: 'bw' only black nad white., 'fc': full color. {value[4]} is default")
 
-contents = []
+# change template in value[3] on real filename
+value[3] = 'img_{:%d%m%Y%H%M%S}_{}x{}.raw'.format(now,value[1],value[0]) 
+
+args = parser.parse_args()
+
+if args.height:
+    value[0] = args.height
+
+if args.width:
+    value[1] = args.width
+
+if args.directory:
+    value[2] = args.directory
+
+if args.filename:
+    value[3] = args.filename
+    if '.' in args.filename:
+        value[3] = value[3][::-1]
+        value[3] = value[3][value[3].find('.') + 1:]
+        value[3] = value[3][::-1]
+    value[3] += '.raw'
+
+if args.colormode:
+    value[4] = args.colormode
+
+if not os.path.exists(value[2]):
+    os.mkdir(value[2])
+
+pathname = value[2] + value[3]
+contets = []
 pixel = 0
 
+
 with open(pathname, 'wb') as fimg:
-    for i in range(height * width):
-        """
-        chc -> wybiera tylko z tych elementów: chc([1, 2, 3, 10]) 
-        rint -> wybiera z zakresu: rnt(1, 20)
-        """
-        pixel = chc([0, 255])
-        contents.append(pixel)
-    fimg.write(bytearray(contents))
+    for i in range (value[0] * value[1]):
+        if value[4] == 'bw':
+            pixel = chc([0, 255])
+        else:
+            pixel = rint(0, 255)
+        contets.append(pixel)
+    fimg.write(bytearray(contets))
